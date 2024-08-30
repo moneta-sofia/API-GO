ade19f15717f746881b5d70690dee8825fcdef8e
elxrojo <elxrojo115@gmail.com>
2024-08-30 17:21:28 -0300

    starting my first GO API :D

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, `{"status": %d, "message": "%s"}`, 200, "YEPIII!")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
