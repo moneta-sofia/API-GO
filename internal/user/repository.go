c8e9fe63b6d77c8e292caa1fa4f9e6423d4672bd
elxrojo <elxrojo115@gmail.com>
2024-09-06 08:43:10 -0300

    fixing getall and update

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -29,16 +29,47 @@ func NewRepository(db *sql.DB, log *log.Logger) Repository {
 }
 
 func (r *repo) Create(ctx context.Context, user *domain.User) error {
-	// r.db.MaxUserID++
-	// user.ID = r.db.MaxUserID
-	// r.db.Users = append(r.db.Users, *user)
-	// r.log.Println("Repository created")
+	sqlQ := "INSERT INTO users(first_name, last_name, email) VALUES (?,?,?)"
+	res, err := r.db.Exec(sqlQ, user.FirstName, user.LastName, user.Email)
+	if err != nil {
+		r.log.Println(err.Error())
+		return err
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		r.log.Println(err.Error())
+		return err
+	}
+	user.ID = uint64(id)
+	r.log.Println("user created with id: ", id)
+
 	return nil
 }
 
 func (r *repo) GetAll(ctx context.Context) ([]domain.User, error) {
-	r.log.Println("Repository get all")
-	return nil, nil
+	var users []domain.User
+	sqlQ := "SELECT id, first_name, last_name, email FROM users"
+	rows, err := r.db.Query(sqlQ)
+	if err != nil {
+		r.log.Println(err.Error())
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var u domain.User
+		if err := rows.Scan(&u.ID, &u.FirstName, &u.LastName, &u.Email); err != nil {
+
+			r.log.Println(err.Error())
+			return nil, err
+		}
+		users = append(users, u)
+	}
+
+	r.log.Println("users get all : ", len(users))
+
+	return users, nil
 }
 
 func (r *repo) Get(ctx context.Context, id uint64) (*domain.User, error) {
